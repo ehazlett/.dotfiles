338189c0601ec7f44d35628b43bbef9e8c43052d
Evan Hazlett <ejhazlett@gmail.com>
2014-06-03 00:32:28 +0000

    updated docker dev shortcut

--- config.fish
+++ config.fish
@@ -49,9 +49,9 @@ end
 
 function d
     if math "1<" (count $argv) > /dev/null
-        docker run -h $argv[1] -it -v ~/Sync:/home/dev/Sync --name $argv[1] -v /var/run/docker.sock:/var/run/docker.sock $argv[2..(count $argv)] ehazlett/devbox fish
+        docker run -h $argv[1] -it -v ~/Sync:/home/dev/Sync --name $argv[1] -v /home/dev -v /var/run/docker.sock:/var/run/docker.sock $argv[2..(count $argv)] ehazlett/devbox fish
     else
-        docker run -h $argv[1] -it -v ~/Sync:/home/dev/Sync --name $argv[1] -v /var/run/docker.sock:/var/run/docker.sock ehazlett/devbox fish
+        docker run -h $argv[1] -it -v ~/Sync:/home/dev/Sync --name $argv[1] -v /home/dev -v /var/run/docker.sock:/var/run/docker.sock ehazlett/devbox fish
     end
 end
 
