69893dd1fc7da45954c1f843613750584c4f6bb1
Evan Hazlett <ejhazlett@gmail.com>
2016-08-12 17:48:07 -0400

    updated xfce4 term
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- linux/monitor.sh
+++ linux/monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-xrandr --output DP1 --auto --above eDP1
-xrandr --output DP1 --scale 1.5x1.5 --mode 2560x1440 --pos 0x0
-xrandr --output eDP1 --scale 1x1 --pos 800x2160
+xrandr --output DP-1 --auto --above eDP-1
+xrandr --output DP-1 --scale 1.5x1.5 --mode 2560x1440 --pos 0x0
+xrandr --output eDP-1 --scale 1x1 --pos 800x2160
 
 feh --bg-scale ~/Sync/home/wallpaper/current
