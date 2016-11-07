755977402ce6d19275d96c3952b47168c6b51e0a
Evan Hazlett <ejhazlett@gmail.com>
2016-11-07 09:05:50 -0500

    update power script; add xmodmap for hhkb
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- linux/vm-powerd.sh
+++ linux/vm-powerd.sh
@@ -2,20 +2,21 @@
 LOG=/var/log/resume-vm.log
 
 echo "$(date)" >> $LOG
+echo "$1" >> $LOG
 
 # resume all vms
 case "$1" in
     suspend)
 	for vm in $(virsh list --name --state-running); do
-	    echo "suspending $vm" >> $LOG
-	    virsh suspend $vm
+	    echo "saving $vm" >> $LOG
+	    virsh managedsave $vm
 	done
 	;;
     resume)
-	sleep 10
-	for vm in $(virsh list --name --state-paused); do
+	sleep 3
+	for vm in $(virsh list --name --state-shutoff --with-managed-save); do
 	    echo "resuming $vm" >> $LOG
-	    virsh resume $vm
+	    virsh start $vm
 	done
 	;;
 esac
