35b3bf1bb694765d6e2152d5dcf3fe17b1e8125e
Evan Hazlett <ejhazlett@gmail.com>
2017-05-18 19:40:39 -0400

    update i3 config; update touchpad script
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- touchpad.sh
+++ touchpad.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 VENDOR=$(cat /sys/class/dmi/id/sys_vendor)
 if [ "$VENDOR" = "Dell Inc." ]; then
-    xinput set-prop 11 274 1
-    xinput set-prop 13 274 1
+    xinput set-prop 11 276 1
+    xinput set-prop 13 276 1
+    xinput set-prop 15 294 1
 elif [ "$VENDOR" = "LENOVO" ]; then
     xinput set-prop 15 292 1
 else
