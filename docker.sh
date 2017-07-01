59f747736c6dc806b1f50a94f1f6c489ed6d4d32
Evan Hazlett <ejhazlett@gmail.com>
2017-07-01 00:01:50 -0400

    update docker script
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- docker.sh
+++ docker.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 DOCKER_VERSION=${DOCKER_VERSION:-"17.06.0"}
+DOCKER_DATA_DIR=${DOCKER_DATA_DIR:-"$HOME/.docker"}
+DIND_NETWORK_NAME=${DIND_NETWORK_NAME:-"dind"}
 
 function launch-node() {
     if [ -z "$1" ]; then
         echo "Usage: 0 <node>"
     fi
     NODE=$1
+    docker network create ${DIND_NETWORK_NAME}
     docker run \
         --privileged \
-        --net dind \
+        --net ${DIND_NETWORK_NAME} \
         --name ${NODE} \
         --hostname ${NODE} \
-        -v /var/tmp/docker-${NODE}:/var/lib/docker \
+        -v ${DOCKER_DATA_DIR}-${NODE}:/var/lib/docker \
         --tmpfs /run \
         -v /lib/modules:/lib/modules:ro \
         -d \
-        ehazlett/dind:17.06.0 -H unix://
+        ehazlett/dind:${DOCKER_VERSION} -H unix://
 }
