d01b0513c0a844a347967ef1202122048c187b0c
Evan Hazlett <ejhazlett@gmail.com>
2018-05-22 02:12:20 -0400

    add go deps
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- config.sh
+++ config.sh
@@ -43,11 +43,6 @@ else
     cd $HOME/.dotfiles && git checkout vimrc
     reset
 
-    go get -v golang.org/x/tools/cmd/goimports
-    go get -v github.com/golang/lint/golint
-    go get -v github.com/LK4D4/vndr
-    go get -v github.com/stevvooe/protobuild
-    go get -v github.com/mdempsky/gocode
 fi
 
 if [ ! -z "$(which protoc)" ];
---
+++ go-deps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+go get -v golang.org/x/tools/cmd/goimports
+go get -v github.com/golang/lint/golint
+go get -v github.com/LK4D4/vndr
+go get -v github.com/stevvooe/protobuild
+go get -v github.com/mdempsky/gocode
