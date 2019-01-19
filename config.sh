4e2c943066127148a4c1951c7b8795c4de91fb61
Evan Hazlett <ejhazlett@gmail.com>
2019-01-19 10:50:58 -1000

    update config
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- config.sh
+++ config.sh
@@ -31,7 +31,8 @@ else
     echo " -> Installing Protobuf support"
     curl -sSL https://github.com/google/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip -o /tmp/protoc.zip
     pushd /usr/local
-    unzip /tmp protoc.zip
+    unzip /tmp/protoc.zip
+    popd
 fi
 
 # User setup
